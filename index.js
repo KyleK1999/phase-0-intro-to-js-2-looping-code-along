c4997fd4c3a9eeb6ebbdeb0a8ea5da556d5c55c3
Kyle <kylekibbe99@gmail.com>
2023-05-26 21:11:01 -0700

    looping code intro

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(names) {
+    const messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+  
+  const names = ["Guadalupe", "Ollie", "Aki"];
+  const messages = writeCards(names);
+  console.log(messages);
+  function countDown(number){
+    for (let i=number; i>=0; i--) {
+        console.log(i);
+    }
+  }
